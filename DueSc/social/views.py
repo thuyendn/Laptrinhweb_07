eea6ac7e6e749b6c3c62ba21c2229ee679cb05e3
Nguyễn Thị Thuỳ Linh <thuylinh311004@gmail.com>
2025-04-06 21:43:48 +0700

    màn hình quản trị và sinh viên

--- DueSc/social/views.py
+++ DueSc/social/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
 
+
+def nhom_da_tham_gia(request):
+    # Lấy dữ liệu hoặc bất kỳ logic nào bạn cần cho trang này
+    return render(request, 'social/Nhom/nhom_da_tham_gia.html')
+def chi_tiet_nhom_dathamgia(request):
+    # Lấy dữ liệu hoặc bất kỳ logic nào bạn cần cho trang này
+    return render(request, 'social/Nhom/chi_tiet_nhom_dathamgia.html')
+def nhom_lam_qtrivien(request):
+    # Lấy dữ liệu hoặc bất kỳ logic nào bạn cần cho trang này
+    return render(request, 'social/Nhom/nhom_lam_qtrivien.html')
+def chi_tiet_nhom_qtrivien(request):
+    # Lấy dữ liệu hoặc bất kỳ logic nào bạn cần cho trang này
+    return render(request, 'social/Nhom/chi_tiet_nhom_qtrivien.html')
+# View cho Bảng tin nhóm
+#def chi_tiet_nhom_qtrivien(request, group_id):
+    #group = Group.objects.get(id=group_id)
+    #return render(request, 'social/Nhom/chi_tiet_nhom_qtrivien.html', {'group': group})
+
+# View cho Phê duyệt thành viên
+def duyet_thanh_vien(request):
+    return render(request, 'social/Nhom/duyet_thanh_vien.html')
+
+# View cho Phê duyệt bài viết
+def duyet_bai_viet(request):
+
+    return render(request, 'social/Nhom/duyet_bai_viet.html')
+def ket_qua_tim_kiem(request):
+    search_query = request.GET.get('search', '')  # Lấy giá trị tìm kiếm từ URL
+    return render(request, 'social/Nhom/group_search_results.html')
+
+# View cho Thành viên của nhóm
+def thanh_vien_nhom(request):
+    return render(request, 'social/Nhom/thanh_vien_nhom.html')
+
+
+
+
+
+
+
+
 def home(request):
     return render(request, 'social/home.html')  # Đảm bảo rằng bạn đang trả về tệp home.html
 
